b73dbe96880ef7f632550c89fefe590483c5da88
phudtaasc-bit <phudt.aasc@gmail.com>
2026-07-10 15:04:12 +0700

    Preserve legacy Sheet 00 entry point after core merge

---
+++ 00_CompatibilityAlias.js
@@ -0,0 +1,7 @@
+/*************************************************
+ * Alias tương thích tạm thời cho trigger/runner cũ.
+ * Logic NPV/IRR đã nằm hoàn toàn trong 00.Tonghop.js.
+ *************************************************/
+function FS_lapSheet00_Patched() {
+  return FS_lapSheet00();
+}
